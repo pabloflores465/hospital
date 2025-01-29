6437740fa3b765098af863d83631c32f57c99897
pabloflores465 <pablopolis2016@gmai.com>
2025-01-29 00:58:49 -0600

    initializing front and backend

---
+++ front/components/button.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { defineProps, PropType } from 'vue'
+
+defineProps({
+    click: {
+        type: Function as PropType<() => void>,
+        required: false
+    },
+    styles: {
+        type: String,
+        required: false
+    },
+    label: {
+        type: String,
+        required: false
+    }
+})
+</script>
+
+<template>
+    <div :class="styles">
+        <button 
+            @click="click" 
+            class="
+                bg-blue-500 
+                hover:bg-blue-600 
+                text-white 
+                font-semibold 
+                py-2 
+                px-4 
+                rounded-full
+                shadow 
+                transition 
+                duration-300 
+                ease-in-out 
+                transform 
+                hover:scale-105
+            "
+        >
+            <slot></slot>
+        </button>
+    </div>
+</template>
---
+++ front/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Button } from '#components';
+function handleClick() {
+  console.log('¡Se hizo click en el botón!');
+}
+</script>
+
+<template>
+    <div class="bg-red-500">
+      adios
+    </div>
+    <Button :click="handleClick" styles="m-3 bg-red-500">
+      que dice
+    </Button>
+  </template>
